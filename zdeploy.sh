bb9ba4c72000f6e714e811dbfeba505ad63b55a8
ej8899 <ej8899@gmail.com>
2025-08-16 14:09:34 -0600

    add: notes to deploy script

--- zdeploy.sh
+++ zdeploy.sh
@@ -1,4 +1,22 @@
 #!/usr/bin/env bash
+
+#
+# zdeploy.sh
+#
+# Purpose:
+#   Push code from the dev server to the homelab web server (.214).
+#   This copies new/changed files into the live site directory but does NOT delete
+#   anything on the destination.
+#
+# Usage:
+#   sh ./zdeploy.sh
+#
+# Notes:
+#   - You will be prompted for the ej account password on .214 (unless we set auth key).
+#   - Adjust the --exclude list as necessary.
+#   - Run from the WEB PUBLIC of your project folder on .213 as it copies to the web PUBLIC on .214.
+#
+
 rsync -avz \
   --exclude ".git" \
   --exclude ".github" \
